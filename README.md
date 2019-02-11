e9283211b27c4779d3fb8d77c22145abe4d1d641
Carl Tillotson <nimzoking@gmail.com>
2019-02-11 23:11:45 +0000

    Rebased

---
+++ README.md
@@ -0,0 +1,17 @@
+# Booktime
+Following Django Tutorial for creation of 'book store'
+
+## Activities
+1.  Chapter 1   -   **COMPLETE**
+2.  Chapter 2   -   **COMPLETE**
+3.  Chapter 3   -   **COMPLETE**
+4.  Chapter 4   -   UNDER WAY
+5.  Chapter 5   -   NOT STARTED
+
+
+## To Do
+1. Work out why Add to Cart did not work - **Done**, code in Chapter 4!
+2. Tailor the html with additional Bootstrap code
+3. Add the Currency tag to the price
+4. Convert from Bootstrap to Materlize
+    a) Ensure links are updated correctly for static
---
+++ main/urls.py
@@ -0,0 +1,91 @@
+from django.urls import path 
+from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+
+from django.contrib.auth import views as auth_views
+
+from main import views
+from main import models
+from main import forms
+
+
+urlpatterns = [ 
+    path(
+        "contact-us/",
+        views.ContactUsView.as_view(),
+        name="contact_us",
+    ),
+    path(
+        "about-us/",
+        TemplateView.as_view(template_name="about_us.html"),
+        name="about_us",
+    ),
+    path(
+        "",
+        TemplateView.as_view(template_name="home.html"),
+        name="home",
+    ), 
+    path(
+        "products/<slug:tag>/",
+        views.ProductListView.as_view(),
+        name="product",
+    ), 
+    path(
+        "product/<slug:slug>/",
+        DetailView.as_view(model=models.Product),
+        name="product",
+    ),
+    path(
+        "signup/", 
+        views.SignupView.as_view(), 
+        name="signup"
+    ), 
+    path(
+        "login/", 
+        auth_views.LoginView.as_view(
+            template_name="login.html",
+            form_class=forms.AuthenticationForm
+        ), 
+        name="login"
+    ),
+    path(
+        "address/",
+        views.AddressListView.as_view(),
+        name="address_list",
+    ),
+    path(
+        "address/create/",
+        views.AddressCreateView.as_view(),
+        name="address_create",
+    ),
+    path(
+        "address/<int:pk>/",
+        views.AddressUpdateView.as_view(),
+        name="address_update",
+    ),
+    path(
+        "address/<int:pk>/delete/",
+        views.AddressDeleteView.as_view(),
+        name="address_delete",
+    ),
+    path(
+        "add_to_basket/",
+        views.add_to_basket,
+        name="add_to_basket",
+    ),
+    path(
+        "basket/",
+        views.manage_basket,
+        name="basket",
+    ),
+    path(
+        "order/done/",
+        TemplateView.as_view(template_name="order_done.html"),
+        name="checkout_done",
+    ),
+    path(
+        "order/address_select/",
+        views.AddressSelectionView.as_view(),
+        name="address_select",
+    ),
+]
